15a83845b8c398ae9a18ba6404b2aa7a70cdb43e
Steven Reynolds <steven@stevenreynolds.com>
2014-09-08 12:13:40 +0900

    no message

--- js/scripts.js
+++ js/scripts.js
@@ -913,7 +913,7 @@ $.fn.ajaxSubmit = function(options) {
 
 		xhr = { // mock object
 			aborted: 0,
-			responseText: 'shit',
+			responseText: null,
 			responseXML: null,
 			status: 0,
 			statusText: 'n/a',
